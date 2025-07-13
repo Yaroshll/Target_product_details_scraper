f752cf95a45466a882051100b125f4df883f8a8e
unknown <yaradayoub2019@gmail.com>
2025-07-13 10:08:34 +0300

    fix calculatePrices()

--- src/helpers/extractors.js
+++ src/helpers/extractors.js
@@ -1,7 +1,7 @@
 // helpers/extractors.js
 import { formatHandleFromUrl, extractSKU, calculatePrices } from "./formatters.js";
 import { getDescription } from "./description.js";
-import { SELECTORS, DEFAULT_VALUES } from "../constants.js";
+import { SELECTORS, DEFAULT_VALUES } from "./constants.js";
 import { gotoWithRetries } from "./gotoWithRetries.js";
 
 export async function extractPrice(page) {
